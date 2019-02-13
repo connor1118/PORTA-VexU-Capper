11cc110ff73efd2b2b3fe970b6e331f434a41df9
connor1118 <connor1118@live.com>
2019-02-13 16:00:40 -0600

    ADJUSTER HOLD

--- src/adjuster.cpp
+++ src/adjuster.cpp
@@ -5,6 +5,7 @@ Motor adjuster(7, MOTOR_GEARSET_18, 0,  MOTOR_ENCODER_DEGREES);
 bool adjPos;
 void adjusterOP()
 {
+  adjuster.set_brake_mode(MOTOR_BRAKE_HOLD);
   if(controller.get_digital(DIGITAL_L2))
   {
     if(adjPos)
@@ -25,6 +26,7 @@ void adjusterOP()
 
 void adjust(bool up)
 {
+  adjuster.set_brake_mode(MOTOR_BRAKE_HOLD);
   if(up)
   {
     adjuster.move_velocity(-200);
